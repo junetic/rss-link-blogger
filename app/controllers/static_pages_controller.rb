0e02648f2e21e1908b5b42c567adfcc51eb9e047
Junu Yang <junetic@gmail.com>
2012-06-22 21:04:32 -0400

    zz

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,10 +3,7 @@ class StaticPagesController < ApplicationController
 		@feed = SimpleRSS.parse open('https://groups.google.com/a/ideo.com/group/iad-ny/feed/rss_v2_0_msgs.xml')
 		#@feed = SimpleRSS.parse open('app/assets/images/rss_v2_0_msgs.xml')
 		@links = []
-<<<<<<< HEAD
-=======
 		@imglink = []
->>>>>>> image scraper loop tweaks
 		@imgs_from_link = []
 		@imgs_from_all_links = []
 		@link_titles = []
@@ -33,10 +30,10 @@ class StaticPagesController < ApplicationController
 					url2=url2.sub('https','http')
 					
 				end
-				@links << url2
+				#@links << url2
 				@links.reject! { |e| e.empty? }
-				#@links_count = @links.count
-				#@links =  ["http://www.qbn.com","http://www.boingboing.net", "http://www.fastcodesign.com", "http://www.nytimes.com","http://www.theverge.com"]
+				#temporary substitute links not using feed
+				@links =  ["http://www.qbn.com","http://www.boingboing.net", "http://www.fastcodesign.com", "http://www.nytimes.com","http://www.theverge.com"]
 			end
 		end
 		@linkcount=@links.count
@@ -49,41 +46,15 @@ class StaticPagesController < ApplicationController
 				linkt = doc.xpath('//title').text
 				@link_titles << linkt
 
-				# #Scrape Images
+				#Scrape Images
 				image_scraper = ImageScraper::Client.new(t)
-<<<<<<< HEAD
-				@imgs_from_link = image_scraper.image_urls[1]
-				@imgs_from_all_links << @imgs_from_link
-				# agent = Mechanize.new
-				# doc = agent.get(url)
-				# agent.get(doc.parser.at('img')['src']).save
-=======
 				@imgs_from_link = image_scraper.image_urls[0..10]
 
 				@imgs_from_all_links << @imgs_from_link
->>>>>>> image scraper loop tweaks
 				
 			end
 		end
 
-<<<<<<< HEAD
-		# def get_largest_image
-
-		# 	#@imgs_from_all_links.each do |t|
-		# 		@imgs_from_link.each do |t|
-		# 			size = FastImage.size(t)
-		# 			@width = size[0]
-		# 			if @width > 100
-		# 				@imglink << t
-		# 			end
-		# 		end
-		# 	#end
-
-		# end
-
-		scrape_links
-		# get_largest_image
-=======
 		def get_largest_image
 			# @size=FastImage.size(@imgs_from_all_links[0][5])
 			0.upto(@imgs_from_all_links.count-1)  do |i|
@@ -103,7 +74,6 @@ class StaticPagesController < ApplicationController
 
 		scrape_links
 		get_largest_image
->>>>>>> image scraper loop tweaks
 
 
 	end
