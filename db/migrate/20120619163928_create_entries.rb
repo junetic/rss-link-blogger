0ba75753701050cd8c4b745a8b8224a1094b9b6b
junetic <junetic@gmail.com>
2012-07-02 11:12:31 -0400

    max height added to css

--- db/migrate/20120619163928_create_entries.rb
+++ db/migrate/20120619163928_create_entries.rb
@@ -3,7 +3,7 @@ class CreateEntries < ActiveRecord::Migration
     create_table :entries do |t|
       t.string :title
       t.string :link
-
+       t.string :imgurl
       t.timestamps
     end
   end
